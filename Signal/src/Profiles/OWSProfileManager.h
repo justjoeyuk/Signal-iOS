8cdc4b2002f10d0e153afdfd9619b89a6d1c73fb
Matthew Chen <charlesmchen@gmail.com>
2017-08-24 18:02:33 -0400

    Show an error when profile name is too long.
    // FREEBIE

--- Signal/src/Profiles/OWSProfileManager.h
+++ Signal/src/Profiles/OWSProfileManager.h
@@ -44,6 +44,8 @@ extern const NSUInteger kOWSProfileManager_MaxAvatarDiameter;
                        success:(void (^)())successBlock
                        failure:(void (^)())failureBlock;
 
+- (BOOL)isProfileNameTooLong:(nullable NSString *)profileName;
+
 #pragma mark - Profile Whitelist
 
 #ifdef DEBUG
